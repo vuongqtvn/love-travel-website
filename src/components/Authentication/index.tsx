f637a622e4959303c108faa86191188a1124c730
Dương Vương <70870745+Vuong-QT@users.noreply.github.com>
2022-10-13 19:04:47 +0700

    change uri

--- src/components/Authentication/index.tsx
+++ src/components/Authentication/index.tsx
@@ -12,8 +12,7 @@ const Authentication = () => {
     const token = localStorage.getItem("token");
     if (token) {
       dispatch(refreshToken(token));
-      // const socket = io("http://localhost:5000");
-      const socket = io("https://love-travel-api-production.up.railway.app");
+      const socket = io("https://love-travel-api.vercel.app");
       console.log({ socket });
       dispatch(setSocket(socket));
 
